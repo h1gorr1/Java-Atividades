564536075cc714e7cc076f3789ec4a7652580ea2
h1gorr <h1.gorr3210@gmail.com>
2026-02-26 15:53:26 -0300

    :)

---
+++ teste java/ex_cargo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ex_cargo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite O Valor Do Salário! ");
+        double salario = sc.nextDouble();
+
+        
+    }
+    
+}
